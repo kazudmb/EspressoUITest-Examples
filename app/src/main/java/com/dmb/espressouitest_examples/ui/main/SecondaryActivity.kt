7c0f0e29dc230118942ebf90af07720fd3d4f760
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-17 17:20:13 +0900

    Initial commit

---
+++ app/src/main/java/com/dmb/espressouitest_examples/ui/main/SecondaryActivity.kt
@@ -0,0 +1,17 @@
+package com.dmb.espressouitest_examples.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dmb.espressouitest_examples.R
+import kotlinx.android.synthetic.main.activity_secondary.*
+
+class SecondaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondary)
+
+        button_back.setOnClickListener {
+            onBackPressed()
+        }
+    }
+}
